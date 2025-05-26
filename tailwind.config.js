471df171248dd9f59b7fe096940ddaca8cc8e92b
aryndraa <aryacoder1102@gmail.com>
2025-05-26 22:04:25 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#111111",
+        secondary: "#ffed4a",
+      },
+    },
+  },
+  plugins: [],
+};
